937b87414ebbe369e44ccc2ee693ae2ee187dfe9
EA - Mariana Silva Andrade <andrade_mariana01@educadventista.org>
2025-05-05 14:01:40 -0300

    função de submit movida para o CardInicial e função de change note movida para a InputButton

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
 
-
 import { useState } from "react"
-import { CardInicial } from "./components/Cardinicial"
+import { CardInicial } from "./components/CardInicial"
 import { CardFinal } from "./components/CardFinal"
 
 export function App(){
   const [submited, setSubmited] =useState(false)
   const [rateNote, setRateNote] = useState(0)
 
-   function handleSubmit(){
-    if(rateNote !== 0){
-      setSubmited(true)
-      return
-    }
    
-    alert("Please,choose a rate button!")
-
- }
-
- function handleChangeRateNote(value){
-    setRateNote(value)
-  }
-   
- 
 
   return(
   
      submited === false ?(
       
-      <CardInicial  handleChangeRateNote={handleChangeRateNote} handleSubmit={handleSubmit}/>
+      <CardInicial setRateNote={setRateNote} setSubmited={setSubmited} rateNote={rateNote}/>
 
      ) : (
       <CardFinal rateNote={rateNote} />
